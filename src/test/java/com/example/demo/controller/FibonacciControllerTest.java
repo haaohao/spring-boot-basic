04392c120ac4d6b4b5c60c101249c9ccd40f9762
LIJO9 <lijo9@oocl.com>
2018-05-29 15:13:55 +0800

    update the Fibonacci test and sonarqube properties

--- src/test/java/com/example/demo/controller/FibonacciControllerTest.java
+++ src/test/java/com/example/demo/controller/FibonacciControllerTest.java
@@ -7,7 +7,7 @@ public class FibonacciControllerTest {
 
   @Test
   public void should_return_1_when_get_fibonacci_given_number_1() throws Exception {
-    Assert.assertEquals((int) new FibonacciController().getFibonacci(1), 2);
+    Assert.assertEquals(1, (int) new FibonacciController().getFibonacci(1));
   }
 
 }
